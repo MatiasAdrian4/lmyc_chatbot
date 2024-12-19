0061c655bc8b39571c721609164fd72c57c6ce74
Matias Adrian Peñalba Paz <matiasadrianpp4@gmail.com>
2024-12-18 22:14:54 -0300

    fix cors issue when calling from the FE

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from routes import messages
 
@@ -7,6 +8,13 @@ app = FastAPI()
 
 app.include_router(messages.router, prefix="/messages", tags=["messages"])
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
